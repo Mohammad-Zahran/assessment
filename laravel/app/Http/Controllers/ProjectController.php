f2e42ee43b3a20a41ec3a1f35347ace0ad7059a2
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-02 11:55:24 +0200

    backend: create addMember function

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\User;
 
 class ProjectController extends Controller
 {
-    public function getproject($id){
+    public function getProject($id){
         $project = Project::find($id);
 
         if(!$project){
@@ -15,5 +16,19 @@ class ProjectController extends Controller
         }
 
         return response() -> json($project);
+    }
+
+    public function addMember($projectId, $userId)
+    {
+        $project = Project::find($projectId);
+        $user = User::find($userId);
+
+        if (!$project || !$user) {
+            return response()->json(['message' => 'Project or User not found'], 404);
+        }
+
+        $project->users()->attach($userId);
+
+        return response()->json(['message' => 'User added to project']);
     }
 }
