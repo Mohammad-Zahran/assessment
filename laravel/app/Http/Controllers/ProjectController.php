dd5e3d23f8174d3c91f628769e5ef34fb1259e00
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-02 11:52:54 +0200

    backend: create getproject function

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function getproject($id){
+        $project = Project::find($id);
+
+        if(!$project){
+            return response() -> json(['message' => 'Project not found'], 404);
+        }
+
+        return response() -> json($project);
+    }
 }
