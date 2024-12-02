de0b885831456741c5d3287fe2b13ce5a33b2cf0
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-02 11:34:06 +0200

    backend: create middleware to track requests

---
+++ laravel/app/Http/Middleware/TrackRequests.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+
+class TrackRequests
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $userId = $request->header('user_id');
+
+        if ($userId) {
+            $user = User::find($userId);
+            if ($user) {
+                $user->increment('requests_num');
+            }
+        }
+
+        return $next($request);
+    }
+}
